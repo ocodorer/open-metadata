dca4a0ecf19ed110f9c3373b8d6e4497fd8e78a7
ocodorer <o.olsson@outlook.com>
2023-05-06 10:30:47 +0300

    Added basic cmd files

---
+++ setup.cmd
@@ -0,0 +1,8 @@
+
+:: Setup open-metadata
+:: https://docs.open-metadata.org/v1.0.0/deployment/docker
+
+:: pip3 install --upgrade 'openmetadata-ingestion[docker]'
+:: metadata docker --start
+
+docker compose up -d
